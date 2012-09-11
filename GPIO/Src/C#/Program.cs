37b367e5af519ee8b13c11c7c9b0151e8a5c07eb
Nived Sivadas <Nived.Sivadas@samraksh.com>
2012-09-11 12:50:46 -0400

    Checking in Timer and GPIO Tests for C#

--- GPIO/Src/C#/Program.cs
+++ GPIO/Src/C#/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using System.Threading;
 
-namespace Template
+namespace TestSuite
 {
     public class Program
     {
+		private static OutputPort testPort = new OutputPort(EmotePins.GPIO_PIN_0, true);
+	
         public static void Main()
         {
-           
+           while(true)
+		   {
+				testPort.Write(true);
+				Thread.Sleep(1000);
+				testPort.Write(false);
+		   }
         }
 
     }
--- Timer/Src/C#/Program.cs
+++ Timer/Src/C#/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using Microsoft.SPOT;
+using System.Threading;
+using Microsoft.SPOT.Hardware;
 
-namespace Template
+namespace TestSuite
 {
     public class Program
     {
+		public static bool state = false;
+		private static OutputPort testPort = new OutputPort(EmotePins.GPIO_PIN_0, true);		
+       
+		
         public static void Main()
         {
+			Timer tmr = new Timer(new TimerCallback(Callback), null, 0, 1000);								
+			
+			while (true)
+            {
+				Thread.Sleep(100);
+            }	
            
         }
+		private static void Callback(Object state)
+        {
+            testPort.Write(true);						
+			testPort.Write(false);
+        }	
 
     }
 }
