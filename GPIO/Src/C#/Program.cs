85560c13dfa40bf1b26d82a3932882179c901743
ChrisAtSamraksh <chris.morse@samraksh.com>
2012-11-07 10:37:54 -0500

    updated GPIO C# test

--- GPIO/Src/C#/Program.cs
+++ GPIO/Src/C#/Program.cs
@@ -2,21 +2,27 @@
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
 using System.Threading;
+using ParameterClass;
 
 namespace TestSuite
 {
     public class Program
     {
-		private static OutputPort testPort = new OutputPort(EmotePins.GPIO_PIN_0, true);
+	private static OutputPort testPort = new OutputPort(EmotePins.GPIO_PIN_0, true);
 	
         public static void Main()
         {
-           while(true)
-		   {
-				testPort.Write(true);
-				Thread.Sleep(1000);
-				testPort.Write(false);
-		   }
+		Parameters parameters = new Parameters();
+		double readFrequency = parameters.frequency;
+		int msThreadSleep = (int)( (1/readFrequency)/2 * 1000);
+
+        	while(true)
+		{
+			testPort.Write(true);
+			Thread.Sleep(msThreadSleep);
+			testPort.Write(false);
+			Thread.Sleep(msThreadSleep);
+		}
         }
 
     }
