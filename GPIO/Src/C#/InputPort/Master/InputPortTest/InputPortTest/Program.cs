4efa589fd9b49b3df8a1cad70d81b909d4359b3f
Nived Sivadas <Nived.Sivadas@samraksh.com>
2013-04-17 16:13:55 -0400

    Checking in inputporttests, globallock and timer rollover bug tests

---
+++ GPIO/Src/C#/InputPort/Master/InputPortTest/InputPortTest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Samraksh.SPOT.Hardware;
+
+namespace Samraksh.SPOT.Tests
+{
+    public class InputPortTestMaster
+    {
+
+        private static OutputPort testPort_PA0 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J11_PIN3, false);
+        private static OutputPort testPort_PA1 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J11_PIN4, false);
+        private static OutputPort testPort_PA2 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J11_PIN5, false);
+        private static OutputPort testPort_PA3 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J11_PIN6, false);
+        private static OutputPort testPort_PA4 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J11_PIN7, false);
+        private static OutputPort testPort_PA8 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J11_PIN8, false);
+        private static OutputPort testPort_PB6 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J11_PIN9, false);
+        private static OutputPort testPort_PB7 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J11_PIN10, false);
+        private static OutputPort testPort_PB8 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J12_PIN1, false);
+        private static OutputPort testPort_PB9 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J12_PIN2, false);
+        private static OutputPort testPort_PB13 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J12_PIN3, false);
+        private static OutputPort testPort_PB14 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J12_PIN4, false);
+        private static OutputPort testPort_PB15 = new OutputPort(Samraksh.SPOT.Hardware.EmoteDotNow.Pins.GPIO_J12_PIN5, false);
+        
+        public static void Main()
+        {
+            Parameters parameters = new Parameters();
+            double readFrequency = parameters.frequency;
+            int msThreadSleep = (int)((1 / readFrequency) / 2 * 1000);
+
+            while (true)
+            {
+                testPort_PA0.Write(true);
+                testPort_PA1.Write(true);
+                testPort_PA2.Write(true);
+                testPort_PA3.Write(true);
+                testPort_PA4.Write(true);
+                testPort_PA8.Write(true);
+                testPort_PB6.Write(true);
+                testPort_PB7.Write(true);
+                testPort_PB8.Write(true);
+                testPort_PB9.Write(true);
+                testPort_PB13.Write(true);
+                testPort_PB14.Write(true);
+                testPort_PB15.Write(true);
+
+               
+                System.Threading.Thread.Sleep(msThreadSleep);
+
+                testPort_PA0.Write(false);
+                testPort_PA1.Write(false);
+                testPort_PA2.Write(false);
+                testPort_PA3.Write(false);
+                testPort_PA4.Write(false);
+                testPort_PA8.Write(false);
+                testPort_PB6.Write(false);
+                testPort_PB7.Write(false);
+                testPort_PB8.Write(false);
+                testPort_PB9.Write(false);
+                testPort_PB13.Write(false);
+                testPort_PB14.Write(false);
+                testPort_PB15.Write(false);
+            
+
+                System.Threading.Thread.Sleep(msThreadSleep);
+            }
+        }
+
+    }
+}
---
+++ Timer/Src/Native/RollOverBug/Timer.cpp
@@ -0,0 +1,142 @@
+#include "Timer.h"
+
+int testCount = 0;
+int secCount=0;
+
+
+TimerTest::TimerTest( UINT32 DisplayIntervalSeconds, UINT32 TimerDurationSeconds )
+{
+	// Initializes the gpio pins
+	//CPU_GPIO_Initialize();
+	//CPU_INTC_Initialize();
+	//HAL_Time_Initialize();
+
+	//Time_Initialize();
+
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) 24, FALSE);
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) 25, FALSE);
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) 1, FALSE);
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) 3, FALSE);
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) 8, FALSE);
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) 29, FALSE);
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) 30, FALSE);
+
+	//Timer_Driver :: Initialize (2 , TRUE, 0, 0, ISR_TIMER, NULL);
+
+	//Timer_Driver :: SetCompare(2 , 1000);
+
+	m_lastRead = 0;
+
+	m_toggleFlag= FALSE;
+
+};
+
+// Test the unsigned int theory
+BOOL TimerTest::Level_0B()
+{
+
+	while(TRUE)
+	{
+		UINT64 lastVal = m_lastRead;
+
+		UINT16 lastSixteenBit = m_lastRead & 0x0000FFFFull;
+
+		UINT16 value = Timer_Driver :: GetCounter (Timer_Driver :: c_SystemTimer);
+
+		m_lastRead &= (0xFFFF0000ull);
+
+		if(Timer_Driver :: DidTimeOverFlow( Timer_Driver::c_SystemTimer ) || (value < lastSixteenBit))
+		{
+
+			CPU_GPIO_EnableOutputPin((GPIO_PIN) 3, TRUE);
+			CPU_GPIO_EnableOutputPin((GPIO_PIN) 3, FALSE);
+
+			Timer_Driver :: ClearTimeOverFlow( Timer_Driver::c_SystemTimer );
+    	//STM_EVAL_LEDToggle((Led_TypeDef)0); //Green
+			m_lastRead += (0x1ull << 16);
+		}
+
+		//Or else the value gets added simply
+		m_lastRead += value;
+
+		if(m_lastRead < lastVal)
+		{
+			CPU_GPIO_SetPinState((GPIO_PIN) 1, TRUE);
+			CPU_GPIO_SetPinState((GPIO_PIN) 1, FALSE);
+		}
+		else
+		{
+			CPU_GPIO_SetPinState((GPIO_PIN) 24, m_toggleFlag);
+			m_toggleFlag = ~m_toggleFlag;
+		}
+
+	}
+
+	return TRUE;
+
+}
+
+BOOL TimerTest::Level_0A()
+{
+	UINT8 i = 0;
+
+	while(i++ < 10)
+	{
+		CPU_GPIO_SetPinState((GPIO_PIN) 24, TRUE);
+		CPU_GPIO_SetPinState((GPIO_PIN) 24, FALSE);
+		CPU_GPIO_SetPinState((GPIO_PIN) 1, TRUE);
+		CPU_GPIO_SetPinState((GPIO_PIN) 1, FALSE);
+		CPU_GPIO_SetPinState((GPIO_PIN) 3, TRUE);
+		CPU_GPIO_SetPinState((GPIO_PIN) 3, FALSE);
+		CPU_GPIO_SetPinState((GPIO_PIN) 8, TRUE);
+	    CPU_GPIO_SetPinState((GPIO_PIN) 8, FALSE);
+	    CPU_GPIO_SetPinState((GPIO_PIN) 25, TRUE);
+	    CPU_GPIO_SetPinState((GPIO_PIN) 25, FALSE);
+	    CPU_GPIO_SetPinState((GPIO_PIN) 29, FALSE);
+	}
+
+
+	while(true)
+	{
+
+		UINT64 curTicks = HAL_Time_CurrentTicks();
+
+
+
+		if(curTicks > m_lastRead)
+		{
+
+			CPU_GPIO_SetPinState((GPIO_PIN) 24, m_toggleFlag);
+			m_toggleFlag = ~m_toggleFlag;
+
+		}
+		else
+		{
+			CPU_GPIO_SetPinState((GPIO_PIN) 1, TRUE);
+			CPU_GPIO_SetPinState((GPIO_PIN) 1, FALSE);
+		}
+
+		m_lastRead = curTicks;
+	}
+
+        return TRUE;
+
+}
+
+void TimerTest :: ISR( void* Param )
+{
+}
+
+BOOL TimerTest::Execute(int level )
+{
+	if(level == 0)
+		return Level_0A();
+	else
+		return Level_0B();
+
+}; //Execute
+
+
+//--//
+
+
