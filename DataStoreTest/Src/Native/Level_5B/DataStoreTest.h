f32d10ef199025d3d91a00a9765c807abae0ff27
Ananth Muralidharan <ananth.muralidharan@samraksh.com>
2013-11-27 13:44:17 -0500

    Added test cases for deletion of records

---
+++ DataStoreTest/Src/Native/Level_5B/DataStoreTest.h
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <D:\AnanthAtSamraksh\MF\MicroFrameworkPK_v4_3\DeviceCode\Include\tinyhal.h>
+#include <D:\AnanthAtSamraksh\MF\MicroFrameworkPK_v4_3\DeviceCode\Include\Samraksh\DataStore\Datastore.h>
+#include <D:\AnanthAtSamraksh\MF\MicroFrameworkPK_v4_3\DeviceCode\Include\Samraksh\HAL_util.h>
+#include <cstdlib>
+#include "testMath.h"
+
+
+#define TEST_INITIALIZATION 0
+#define TESTREADWRITE_DELETE_MULTIPLE_SEQ_RECORDS 1
+#define LEVEL_0A 2
+#define LEVEL_0C  3
+#define LEVEL_1 4
+
+class DataStoreTest
+{
+
+	int numberOfEvents;
+
+	testMath testMathInstance;
+
+public:
+
+//--//
+    DataStoreTest ( int seedValue, int numberOfEvents );
+
+    BOOL     Execute( int testLevel );
+
+	BOOL 	 Level_0A();
+
+	BOOL     Level_0C();
+
+	BOOL 	 Level_1();
+
+	BOOL 	 test_initialization();
+	BOOL 	 TestReadWrite_Delete_Multiple_Seq_Records();
+	LPVOID*	 CreateDataStoreRecords(int);
+	BOOL	 DeleteDataStoreRecords(RECORD_ID);
+	LPVOID	 GetAddress(RECORD_ID);
+	RECORD_ID	 GetRecordID(LPVOID);
+
+	BOOL     DisplayStats(BOOL result, char* resultParameter1, char* resultParameter2, int accuracy);
+
+};
+
+
+
+
+
