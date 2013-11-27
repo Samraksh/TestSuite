c68152e0895f52b61f0bfa7bacd17751d2737aa4
Ananth Muralidharan <ananth.muralidharan@samraksh.com>
2013-11-26 21:36:18 -0500

    Adding test case that uses virtual timer

---
+++ DataStoreTest/Src/Native/Level_4A/DataStoreTest.h
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <D:\AnanthAtSamraksh\MF\MicroFrameworkPK_v4_3\DeviceCode\Include\tinyhal.h>
+#include <D:\AnanthAtSamraksh\MF\MicroFrameworkPK_v4_3\DeviceCode\Include\Samraksh\DataStore\Datastore.h>
+#include <D:\AnanthAtSamraksh\MF\MicroFrameworkPK_v4_3\DeviceCode\Include\Samraksh\HAL_util.h>
+#include <D:\AnanthAtSamraksh\MF\MicroFrameworkPK_v4_3\DeviceCode\Include\Samraksh\HALTimer.h>
+#include <cstdlib>
+#include "testMath.h"
+
+
+#define TEST_INITIALIZATION 0
+#define TESTREADWRITE_VIRTUAL_RECORDS 1
+#define TESTDELETE 2
+#define TESTGETRECORDID 3
+#define LEVEL_0A 4
+#define LEVEL_0C  5
+#define LEVEL_1 6
+
+class DataStoreTest
+{
+
+	int numberOfEvents;
+
+	testMath testMathInstance;
+
+public:
+
+//--//
+	DataStoreTest();
+
+	DataStoreTest ( int seedValue, int numberOfEvents );
+
+
+    BOOL     Execute( int testLevel );
+
+	BOOL 	 Level_0A();
+
+	BOOL     Level_0C();
+
+	BOOL 	 Level_1();
+
+	BOOL 	 test_initialization();
+	BOOL 	 TestReadWrite_Virtual_Records();
+	LPVOID 	 CreateDataStoreRecords(int);
+	BOOL	 DeleteDataStoreRecords();
+	LPVOID	 GetAddress(RECORD_ID);
+	BOOL	 GetRecordID();
+	BOOL	 Read_And_Write();
+
+	BOOL     DisplayStats(BOOL result, char* resultParameter1, char* resultParameter2, int accuracy);
+
+	UINT16	 GenerateRandomNumber(UINT16);
+	/*void	 Timer_1_Handler(LPVOID);
+	void	 Timer_2_Handler(LPVOID);
+	void	 Timer_3_Handler(LPVOID);
+	void	 Timer_4_Handler(LPVOID);*/
+
+};
+
+
+
+
+
---
+++ DataStoreTest/Src/Native/Level_4A/EntryPoint.cpp
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) The Samraksh Company.  All rights reserved.
+// This is an auto generated file by the test create tool
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define __regtest
+
+#include <D:\AnanthAtSamraksh\MF\MicroFrameworkPK_v4_3\DeviceCode\Include\tinyhal.h>
+#include "DataStoreTest.h"
+
+
+void Keep_Linker_Happy() {
+	BootstrapCode();
+	BootEntry();
+}
+
+
+HAL_DECLARE_NULL_HEAP();
+
+void PostManagedEvent( UINT8 category, UINT8 subCategory, UINT16 data1, UINT32 data2 )
+{
+}
+
+void ApplicationEntryPoint()
+{
+    BOOL result;
+    
+
+    do
+    {
+    	DataStoreTest dsTestObject(0,100);
+
+    	dsTestObject.Execute(TESTREADWRITE_VIRTUAL_RECORDS);
+
+    } while(false); // run only once!
+
+    while(true);
+}
