ade46e3731a5aadcadb6ce3e3622d7fc8bd4ff90
ChrisAtSamraksh <chris.morse@samraksh.com>
2014-04-23 14:57:52 -0400

    Events_SetBoolTimer test check in

---
+++ TimeFunctions/Src/Native/Events_SetBoolTimer/EntryPoint.cpp
@@ -0,0 +1,46 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) The Samraksh Company.  All rights reserved.
+// Author: Mukundan Sridharan
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define __regtest
+
+#include <tinyhal.h>
+#include "Parameters.h"
+#include <i2c/stm32f10x_i2c.h>
+#include <rcc/stm32f10x_rcc.h>
+#include <gpio/stm32f10x_gpio.h>
+
+void Keep_Linker_Happy() {
+	BootstrapCode();
+	BootEntry();
+}
+
+
+HAL_DECLARE_NULL_HEAP();
+
+void PostManagedEvent( UINT8 category, UINT8 subCategory, UINT16 data1, UINT32 data2 )
+{
+}
+
+void ApplicationEntryPoint()
+{
+	UINT32 wait = (UINT32)( (1/frequency)/2 * 1000);
+	BOOL m_timeQuantumExpired;
+	
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) 24, TRUE);
+
+    while (TRUE){
+		m_timeQuantumExpired = false;
+		CPU_GPIO_SetPinState((GPIO_PIN) 24, FALSE);		
+		::Events_SetBoolTimer( (BOOL*)&m_timeQuantumExpired, wait );	
+		while (m_timeQuantumExpired == false) {}
+		m_timeQuantumExpired = false;
+		CPU_GPIO_SetPinState((GPIO_PIN) 24, TRUE);
+		::Events_SetBoolTimer( (BOOL*)&m_timeQuantumExpired, wait );	
+		while (m_timeQuantumExpired == false) {}
+	}
+
+}
+
---
+++ TimeFunctions/Src/Native/Events_SetBoolTimer/Parameters.h
@@ -0,0 +1,22 @@
+﻿// required TestRig parameters
+#define testTimeout 60000
+#define useLogic "normal"
+#define sampleTimeMs 10000
+#define sampleFrequency 4000000
+#define useCOMPort false
+#define forceCOM ""
+#define COMParameters "115200,N,8,1"
+#define useTestScript false
+#define testScriptName "none.tst"
+#define testScriptTimeoutMs 9000
+#define useAnalysis "Matlab"
+#define	analysisScriptName "analyze_h.m"
+#define	useResultsFile false
+#define resultsFileName "results.txt"
+// Do not change text format above this point
+
+// test specific parameters
+#define frequency 50
+#define upperAllowedFrequency 64
+#define lowerAllowedFrequency 46 
+#define expectedFrequency 50
