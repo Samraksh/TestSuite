128456c1188c851f1067a085682a6235983e1e05
Bora Karaoglu <bora.karaoglu@samraksh.com>
2015-11-30 12:53:38 -0500

    TimerModificationTest: This is adding a simple test in which a timer is changed to follow a schedule similar to Discovery module of the OMAC. When the timer fires,  A PIN is toggled and timer's next instance is modified. We can reduce the number of timers in OMAC scheduler from 2 to 1 if this succeeds.

---
+++ OMAC/Src/Native/TimerModificationTest/OMACTest.cpp
@@ -0,0 +1,86 @@
+/*
+ * TimerModificationTest.cpp
+ */
+#include <tinyhal.h>
+//#include <Samraksh/HALTimer.h>
+#include <Samraksh/VirtualTimer.h>
+#include "OMACTest.h"
+
+
+#define NEIGHBORCLOCKMONITORPIN 31 //2
+#define LOCALCLOCKMONITORPIN 22 //25
+
+#define LocalClockMonitor_TIMER1 32
+#define NeighborClockMonitor_TIMER1 33
+
+#define USEONESHOTTIMER FALSE
+
+//NEIGHBORCLOCKMONITORPERIOD in ticks
+#define NEIGHBORCLOCKMONITORPERIOD 4000000
+#define INITIALDELAY 100000
+
+void CMaxTSLocalClockMonitorTimerHandler(void * arg) {
+	//Toggle Pin State for monitoring with Logic Analyzer
+	CPU_GPIO_SetPinState((GPIO_PIN) LOCALCLOCKMONITORPIN, TRUE);
+	BOOL rv = gOMACTest.ScheduleNextLocalCLK();
+	CPU_GPIO_SetPinState((GPIO_PIN) LOCALCLOCKMONITORPIN, FALSE);
+}
+
+
+
+// TIMESYNCTEST
+
+BOOL OMACTest::Initialize(){
+
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) NEIGHBORCLOCKMONITORPIN, TRUE);
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) LOCALCLOCKMONITORPIN, TRUE);
+	CPU_GPIO_SetPinState((GPIO_PIN) NEIGHBORCLOCKMONITORPIN, FALSE);
+	CPU_GPIO_SetPinState((GPIO_PIN) LOCALCLOCKMONITORPIN, FALSE);
+
+
+	VirtTimer_Initialize();
+
+
+	m_period1 = 37;
+	m_period2 = 191;
+
+	VirtualTimerReturnMessage rm;
+	rm = VirtTimer_SetTimer(LocalClockMonitor_TIMER1, 0, NEIGHBORCLOCKMONITORPERIOD, USEONESHOTTIMER, FALSE, CMaxTSLocalClockMonitorTimerHandler);
+	ASSERT_SP(rm == TimerSupported);
+
+	return TRUE;
+}
+
+BOOL OMACTest::StartTest(){
+	VirtualTimerReturnMessage rm;
+	UINT64 y = HAL_Time_CurrentTicks();
+	UINT64 LocalClockMonitorFrameNum = y /( (UINT64) NEIGHBORCLOCKMONITORPERIOD );
+	ScheduleNextLocalCLK();
+	rm = VirtTimer_Start(LocalClockMonitor_TIMER1);
+	return TRUE;
+}
+
+
+
+BOOL OMACTest::ScheduleNextLocalCLK(){
+		VirtualTimerReturnMessage rm;
+		UINT64 currentSlotNum, period1Remaining, period2Remaining;
+		UINT64 y = HAL_Time_CurrentTicks();
+		currentSlotNum = y / (16 * 8000);
+		period1Remaining = currentSlotNum % m_period1;
+		period2Remaining = currentSlotNum % m_period2;
+		period1Remaining = ((period1Remaining < period2Remaining) ? (period1Remaining ) : (period2Remaining ));
+		UINT32 MicSTillNextEvent = period1Remaining * 16 * 1000;
+		rm = VirtTimer_Change(LocalClockMonitor_TIMER1, 0, MicSTillNextEvent, USEONESHOTTIMER);
+		ASSERT_SP(rm == TimerSupported);
+		return TRUE;
+
+}
+
+void OMACTest_Initialize(){
+	BOOL ret;
+	ret = gOMACTest.Initialize();
+	ret = gOMACTest.StartTest();
+}
+
+
