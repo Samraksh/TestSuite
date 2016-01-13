9c929ec801b6c96fd823795720b658296d326ecf
Ananth Muralidharan <ananth.muralidharan@samraksh.com>
2016-01-13 11:17:05 -0500

    Checks in test to verify h/w acks (RF231's extended operating mode)

---
+++ Radio/Src/Native/ExtendedModeTest/Send/RadioTestSend.cpp
@@ -0,0 +1,148 @@
+/*
+ * TimesyncTest.cpp
+ */
+
+/*
+ * RadioTestSend.cpp
+ *
+ *  Created on: Jan 13, 2016
+ *      Author: Ananth Muralidharan
+ *      A test to verify the working of RF231's extended operating mode
+ */
+
+#include "RadioTestSend.h"
+
+
+const UINT16 ONESEC_IN_MSEC = 1000;
+const UINT16 ONEMSEC_IN_USEC = 1000;
+
+RadioTestSend g_RadioTestSend;
+Buffer_15_4_t g_send_buffer;
+extern RF231Radio grf231Radio;
+
+#define DEBUG_RadioTest 1
+#define TEST_0A_TIMER1	10
+#define TEST_0A_TIMER2	11
+#define TIMER1_PERIOD 	ONESEC_IN_MSEC
+#define TIMER2_PERIOD 	100
+#define Test_0A_Timer_Pin (GPIO_PIN)31 //2
+
+
+/*
+ * Keep sending a packet every x amount of time until a response is received
+ */
+void Test_0A_Timer1_Handler(void * arg){
+	hal_printf("Test_0A_Timer1_Handler; Sending msgID: %d\n", g_RadioTestSend.msgID);
+	g_RadioTestSend.SendPacket();
+}
+
+void Test_0A_Timer2_Handler(void * arg){
+	hal_printf("Test_0A_Timer2_Handler; No h/w ack received\n");
+}
+
+void* RadioTest_ReceiveHandler (void* msg, UINT16 size){
+	hal_printf("RadioTest_ReceiveHandler; msg received\n");
+
+	//If h/w ack is received, stop timer2 and increment msgID
+	VirtTimer_Stop(TEST_0A_TIMER2);
+	g_RadioTestSend.msgID++;
+	return g_RadioTestSend.Receive(msg, size);
+}
+
+void RadioTest_SendAckHandler (void* msg, UINT16 size, NetOpStatus status){
+	hal_printf("RadioTest_SendAckHandler; msg sent\n");
+}
+
+BOOL RadioTest_InterruptHandler(RadioInterrupt Interrupt, void *param){
+	hal_printf("RadioTest_InterruptHandler\n");
+}
+
+/*
+ * As soon as a response is received from another node, start the 2nd timer
+ */
+void* RadioTestSend::Receive(void* tmpMsg, UINT16 size)
+{
+	Message_15_4_t* rcvdMsg = (Message_15_4_t*)tmpMsg;
+	/*if(!initialPacketReceived){
+		initialPacketReceived = true;
+		////VirtTimer_Start(TEST_0A_TIMER2);
+	}*/
+}
+
+void RadioTestSend::CreatePacket()
+{
+	IEEE802_15_4_Header_t *header = msg_carrier.GetHeader();
+
+	header->length = sizeof(Payload_t) + sizeof(IEEE802_15_4_Header_t);
+	header->fcf = (65 << 8);
+	header->fcf |= 136;
+	header->dsn = 97;
+	header->destpan = (34 << 8);
+	header->destpan |= 0;
+	header->src = CPU_Radio_GetAddress(this->radioName);
+}
+
+void RadioTestSend::SendPacket()
+{
+	Payload_t* data_msg = (Payload_t*)msg_carrier.GetPayload();
+	msg.MSGID = msgID;
+	for(int i = 1; i <= PAYLOAD_SIZE; i++){
+		msg.data[i-1] = i;
+	}
+	*data_msg = msg;
+
+	CPU_GPIO_SetPinState((GPIO_PIN) Test_0A_Timer_Pin, TRUE);
+	(Message_15_4_t*)grf231Radio.Send_TimeStamped(&msg_carrier, (msg_carrier.GetHeader())->GetLength(), HAL_Time_CurrentTicks());
+	CPU_GPIO_SetPinState((GPIO_PIN) Test_0A_Timer_Pin, FALSE);
+
+	//After sending msg, start timer2 which waits for a h/w ack from sender.
+	VirtTimer_Start(TEST_0A_TIMER2);
+}
+
+/*
+ * Start TEST_0A_TIMER1 which keeps sending a packet with same msgID until a h/w ack is received
+ */
+BOOL RadioTestSend::StartTest()
+{
+	VirtualTimerReturnMessage rm;
+	rm = VirtTimer_Start(TEST_0A_TIMER1);
+	ASSERT(rm == TimerSupported);
+
+	return TRUE;
+}
+
+BOOL RadioTestSend::Initialize()
+{
+	CPU_GPIO_EnableOutputPin((GPIO_PIN) Test_0A_Timer_Pin, TRUE);
+	CPU_GPIO_SetPinState((GPIO_PIN) Test_0A_Timer_Pin, FALSE);
+
+	//initialPacketReceived = false;
+	radioName = RF231RADIO;
+	DeviceStatus status;
+	Radio_Event_Handler.SetRadioInterruptHandler(RadioTest_InterruptHandler);
+	Radio_Event_Handler.SetSendAckHandler(RadioTest_SendAckHandler);
+	Radio_Event_Handler.SetReceiveHandler(RadioTest_ReceiveHandler);
+	status = grf231Radio.Initialize(&Radio_Event_Handler, this->radioName, 1);
+	//grf231Radio.TurnOnRx();
+
+	//Weird! VirtTimer_Initialize is needed to run this test in master branch, but not needed while running in OMAC_Master_Final branch
+	VirtTimer_Initialize();
+	VirtualTimerReturnMessage rm;
+	rm = VirtTimer_SetTimer(TEST_0A_TIMER1, 0, TIMER1_PERIOD*ONEMSEC_IN_USEC, FALSE, FALSE, Test_0A_Timer1_Handler);
+	ASSERT(rm == TimerSupported);
+	rm = VirtTimer_SetTimer(TEST_0A_TIMER2, 0, TIMER2_PERIOD*ONEMSEC_IN_USEC, TRUE, FALSE, Test_0A_Timer2_Handler);
+	ASSERT(rm == TimerSupported);
+
+	msgID = 1;
+	CreatePacket();
+
+	return TRUE;
+}
+
+void RadioTest_Initialize()
+{
+	g_RadioTestSend.Initialize();
+	g_RadioTestSend.StartTest();
+}
+
+
---
+++ Radio/Src/Native/ExtendedModeTest/Send/RadioTestSend.h
@@ -0,0 +1,52 @@
+/*
+ * RadioTest.h
+ */
+
+#ifndef RadioTest_H_
+#define RadioTest_H_
+
+#include <tinyhal.h>
+#include <Samraksh/VirtualTimer.h>
+#include <Samraksh/Message.h>
+#include <pal/COM/Wireless/MAC/CSMAMAC/csmaMAC.h>
+#include <Targets/Native/STM32F10x/DeviceCode/drivers/radio/RF231/RF231.h>
+
+
+#define PAYLOAD_SIZE	5
+
+typedef struct  {
+	UINT16 MSGID;
+	UINT8 data[PAYLOAD_SIZE];
+}Payload_t;
+
+class RadioTestSend	{
+public:
+	Payload_t msg;
+	Message_15_4_t msg_carrier;
+	UINT8 radioName;
+	UINT32 msgID;
+
+	RadioEventHandler_t Radio_Event_Handler;
+	/*bool initialPacketReceived;
+	UINT8 MyAppID;
+	MacConfig Config;
+	UINT8 MacId;
+	MacEventHandler myEventHandler;*/
+
+	BOOL Initialize();
+	BOOL StartTest();
+	void SendPacket();
+	void CreatePacket();
+	void* Receive(void* msg, UINT16 size);
+	BOOL Send();
+	void SendAck(void *msg, UINT16 size, NetOpStatus status);
+};
+
+//extern RadioTest g_RadioTest;
+
+void RadioTest_Initialize();
+
+#endif /* RadioTest_H_ */
+
+
+
