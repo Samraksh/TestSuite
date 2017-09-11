aef5ba4f93c9484ecabcd4413a452834cc55ea48
Bora Karaoglu <bora.karaoglu@samraksh.com>
2017-09-11 12:38:56 -0400

    Adding a inheritance test

---
+++ Misc/RequestSendAtTimeInstantTest/EntryPoint.cpp
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) The Samraksh Company.  All rights reserved.
+// This is an auto generated file by the test create tool
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define __regtest
+
+#include <tinyhal.h>
+#include "RequestSendAtTimeInstantTest.h"
+
+
+void Keep_Linker_Happy() {
+	BootstrapCode();
+	BootEntry();
+}
+
+
+HAL_DECLARE_NULL_HEAP();
+
+void PostManagedEvent( UINT8 category, UINT8 subCategory, UINT16 data1, UINT32 data2 )
+{
+}
+
+void ApplicationEntryPoint()
+{
+    BOOL result;
+    
+    do
+    {
+    	Test_InitializeAndRun();
+
+    	//dsTestObject.Execute(TEST_INITIALIZATION);
+
+    } while(false); // run only once!
+
+    while(true){
+    	::Events_WaitForEvents(0, 100);
+    }
+}
