7fd08721b2062fa00ce287354a62dfa649b8864a
Bora Karaoglu <bora.karaoglu@samraksh.com>
2017-09-12 18:02:32 -0400

    Adding Chris's inheritanceTest

--- Misc/InheritanceTest/InheritanceTestObjectt.h
+++ Misc/InheritanceTest/InheritanceTestObjectt.h
@@ -14,31 +14,28 @@ namespace InheritanceTest{
 //namespace A{
 class Base1{
 public:
-	virtual ~Base1();
-	void callfoo_virt1(){
-		foo_virt1();
-	}
-	virtual void foo_virt1();
+	Base1() { hal_printf("Base1 constructor\r\n"); }
+	~Base1(){ hal_printf("destroying base\r\n"); }
+	void callfoo_virt1(){foo_virt1();}
+	virtual void foo_virt1(){ hal_printf("base1 foo_virt1\r\n"); }
 };
-//};
-
-
-
 
 class Der1 : public Base1 {
 public:
-	virtual ~Der1();
-	virtual void foo_virt1() ;
-	void foo_virt2();
-	void Run();
+	Der1(){ hal_printf("Der1 constructor\r\n"); }
+	~Der1(){ hal_printf("destroying der1\r\n"); }
+	virtual void foo_virt1() { hal_printf("Der1 foo virt1\r\n"); }
+	void foo_virt2(){hal_printf("Der1::foo_virt2 \r\n");}
+	void Run(){ callfoo_virt1(); }
 };
 
 class InheritanceTestObject_t : public Der1{
 public:
-	InheritanceTestObject_t();
-	virtual ~InheritanceTestObject_t();
-
+	InheritanceTestObject_t(){ hal_printf("InheritanceTestObject_t constructor\r\n"); }
+	void foo_virt1(){ hal_printf("InheritanceTestObject_t::foo_virt1 \r\n"); }
+	~InheritanceTestObject_t(){ hal_printf("destroying InheritanceTestObject_t\r\n"); }
 };
+
 
 void InheritanceTestTest_InitializeAndRun();
 
