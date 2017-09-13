1b0f5dcc327a9273b228e65fe50d3aa2139ea061
Bora Karaoglu <bora.karaoglu@samraksh.com>
2017-09-13 11:46:11 -0400

    Implementations moved to cpp file

--- Misc/InheritanceTest/InheritanceTestObjectt.cpp
+++ Misc/InheritanceTest/InheritanceTestObjectt.cpp
@@ -9,6 +9,20 @@
 
 namespace InheritanceTest{
 
+Base1::Base1(){ hal_printf("Base1 constructor\r\n"); }
+Base1::~Base1(){ hal_printf("destroying base\r\n"); }
+void Base1::callfoo_virt1(){foo_virt1();}
+//void Base1::foo_virt1(){ hal_printf("base1 foo_virt1\r\n"); }
+
+Der1::Der1(){ hal_printf("Der1 constructor\r\n"); }
+Der1::~Der1(){ hal_printf("destroying der1\r\n"); }
+void Der1::foo_virt1() { hal_printf("Der1 foo virt1\r\n"); }
+void Der1::foo_virt2(){hal_printf("Der1::foo_virt2 \r\n");}
+void Der1::Run(){ callfoo_virt1(); }
+
+InheritanceTestObject_t::InheritanceTestObject_t(){ hal_printf("InheritanceTestObject_t constructor\r\n"); }
+void InheritanceTestObject_t::foo_virt1(){ hal_printf("InheritanceTestObject_t::foo_virt1 \r\n"); }
+InheritanceTestObject_t::~InheritanceTestObject_t(){ hal_printf("destroying InheritanceTestObject_t\r\n"); }
 
 InheritanceTestObject_t gInheritanceTest;	
 Der1 gder1;
@@ -33,13 +47,13 @@ void InheritanceTestTest_InitializeAndRun() {
 	p->foo_virt1();
 	for(UINT8 i = 0; i < limit; ++i){}
 
-	p->Base1::foo_virt1();
+//	p->Base1::foo_virt1();
 	for(UINT8 i = 0; i < limit; ++i){}
 
 	gInheritanceTest.foo_virt1();
 	for(UINT8 i = 0; i < limit; ++i){}
 
-	gInheritanceTest.Run();
+//	gInheritanceTest.Run(); //BK: Segfaults in this line
 }
 
 
