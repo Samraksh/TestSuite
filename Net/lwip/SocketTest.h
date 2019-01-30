cf75a1b11825833a2a4d72817b25450214804837
Mukundan Sridharan <mukundan.sridharan@samraksh.com>
2019-01-29 23:14:03 -0500

    Checking in a ping test for lwip socket

---
+++ Net/lwip/SocketTest.h
@@ -0,0 +1,22 @@
+#ifndef _SOCKETTEST_H_
+#define _SOCKETTEST_H_
+
+#include <PKCS11\CryptokiPAL.h>
+#include <tinyhal.h>
+
+
+class SocketTest
+{
+	//char largeArray[512];
+public:
+
+//--//
+	SocketTest ( );
+    BOOL   Execute();
+
+};
+
+
+
+
+#endif //_SOCKETTEST_H_
